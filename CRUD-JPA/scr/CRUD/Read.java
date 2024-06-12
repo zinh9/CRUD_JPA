6a7a80b79db3cafe223197ef56b4b72ba14d2b64
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2024-06-12 18:05:21 -0300

    Update Read.java

--- CRUD-JPA/scr/CRUD/Read.java
+++ CRUD-JPA/scr/CRUD/Read.java
@@ -3,26 +3,32 @@ package CRUD;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 import modelo.Pessoa;
+import infra.DAO;
 
 public class Read {
 	public static void motrarPessoa(Long id) {
 		if(id instanceof Long) {
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
-			EntityManager em = emf.createEntityManager();
-			
-			Pessoa pessoa = em.find(Pessoa.class, id);
-			
-			System.out.println("ID: " + pessoa.getId());
-			System.out.println("Nome: " + pessoa.getNome());
-			System.out.println("Sobrenome: " + pessoa.getSobrenome());
-			System.out.println("CPF: " + pessoa.getCpf());
-			System.out.println("Email: " + pessoa.getEmail());
-			System.out.println("Idade: " + pessoa.getIdade());
-			
-			em.close();
-			emf.close();
+			DAO<Pessoa> dao = new DAO<Pessoa>(Pessoa.class);
+
+			List<Pessoa> pessoas = dao.getAll();
+
+			for(Pessoa pessoa : pessoas){
+				if(pessoa.getId() == id){
+					System.out.println("ID: " + pessoa.getId());
+					System.out.println("Nome: " + pessoa.getNome());
+					System.out.println("Sobrenome: " + pessoa.getSobrenome());
+					System.out.println("CPF: " + pessoa.getCpf());
+					System.out.println("Email: " + pessoa.getEmail());
+					System.out.println("Idade: " + pessoa.getIdade());
+				} else {
+					System.out.println("Pessoa não encontrada!");
+				}
+			}
+
+			dao.close();
 		} else {
 			System.out.println("O ID não é do tipo necessario para continuar a operação!");
 		}
