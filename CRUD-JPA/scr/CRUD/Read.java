3f07c5c8a06bd47521230bc7bf3b4261d8494c6c
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2024-06-12 18:10:18 -0300

    Update Read.java

--- CRUD-JPA/scr/CRUD/Read.java
+++ CRUD-JPA/scr/CRUD/Read.java
@@ -1,10 +1,5 @@
 package CRUD;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.List;
-
 import modelo.Pessoa;
 import infra.DAO;
 
