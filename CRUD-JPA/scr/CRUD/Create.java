3f6871a395ce3e9cb79c758ad3813eaa0849409e
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2024-06-12 18:10:40 -0300

    Update Create.java

--- CRUD-JPA/scr/CRUD/Create.java
+++ CRUD-JPA/scr/CRUD/Create.java
@@ -1,9 +1,5 @@
 package CRUD;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import infra.DAO;
 import modelo.Pessoa;
 
