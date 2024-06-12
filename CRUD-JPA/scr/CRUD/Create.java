01afa84ef64ce8e21c63a422f937a781380ef658
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2024-06-12 17:59:44 -0300

    Update Create.java

--- CRUD-JPA/scr/CRUD/Create.java
+++ CRUD-JPA/scr/CRUD/Create.java
@@ -4,29 +4,25 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import infra.DAO;
 import modelo.Pessoa;
 
 public class Create {
 
 	public static void inserirPessoa(String nome, String sobrenome, String cpf, String email, int idade) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
-		EntityManager em = emf.createEntityManager();
+		DAO<Pessoa> dao = new DAO<Pessoa>(Pessoa.class);
 		
 		Pessoa pessoa = new Pessoa(nome, sobrenome, cpf, email, idade);
 		
 		if(pessoa != null) {
-			em.getTransaction().begin();
-			em.persist(pessoa);
-			em.getTransaction().commit();
+			dao.insertAtomic(pessoa);
 			
 			System.out.println("Pessoa inserida com sucesso!");
 		} else {
 			System.out.println("Não foi possivel inserir a pessoa!");
 		}
-		
-		
-		em.close();
-		emf.close();
+
+		dao.close();
 	}
 
 }
