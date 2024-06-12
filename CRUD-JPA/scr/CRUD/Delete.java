352ce6d0a6d708db827cff97c6cf57ffcabe74ac
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2024-06-12 18:10:27 -0300

    Update Delete.java

--- CRUD-JPA/scr/CRUD/Delete.java
+++ CRUD-JPA/scr/CRUD/Delete.java
@@ -1,9 +1,5 @@
 package CRUD;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import modelo.Pessoa;
 import infra.DAO;
 
