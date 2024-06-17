d400e627c7d7b15d99973de4aa412a93aa367ae6
Enzo Spadetto Rufino <127460831+zinh9@users.noreply.github.com>
2024-06-17 20:17:17 -0300

    Update Contato.java

--- CRUD-JPA/scr/modelo/Contato.java
+++ CRUD-JPA/scr/modelo/Contato.java
@@ -1,49 +1,72 @@
 import java.util.List;
 import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
 
 @Entity
 @Table(name = "contatos")
-public class Contato{
-  @Id
-  @GeneredValue(strategy = GeneratedType.IDENTITY)
-  private Long id;
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
   
-  private String tipo;
+    private String tipo;
   
-  private String numero;
+    private String numero;
 
-  @OneToMany
-  private List<Pessoa> pessoas = new ArrayList<>();
+    @OneToMany
+    private List<Pessoa> pessoas = new ArrayList<>();
 
-  public Contato(){
-  }
+    public Contato() {
+    }
 
-  public Contato(String tipo, String numero){
-    this.tipo = tipo;
-    this.numero = numero;
-  }
+    public Contato(String tipo, String numero) {
+        this.tipo = tipo;
+        this.numero = numero;
+    }
 
-  public Long getId(){
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id){
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getTipo(){
-    return tipo;
-  }
+    public String getTipo() {
+        return tipo;
+    }
 
-  public void setTipo(String tipo){
-    this.tipo = tipo;
-  }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
-  public String getNumero(){
-    return numero;
-  }
+    public String getNumero() {
+        return numero;
+    }
 
-  public void setNumero(String numero){
-    this.numero = numero;
-  }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+
+    public void adicionarPessoa(Pessoa pessoa) {
+        pessoas.add(pessoa);
+    }
+
+    public void removerPessoa(Pessoa pessoa) {
+        pessoas.remove(pessoa);
+    }
 }
